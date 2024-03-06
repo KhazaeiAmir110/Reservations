8de911f93a47f31896d4a6d2e862ca10d89c1f0d
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-03-06 19:33:10 +0330

    add register page

--- reservations/urls.py
+++ reservations/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('user/', include('apps.userauths.urls'), name='userauths'),
     path('', include('apps.company.urls'), name='company')
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
