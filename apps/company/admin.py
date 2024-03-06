e51039f8d1b7e5dfafab2082475f8ffe80b9a16d
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-03-07 00:04:21 +0330

    create models company , category and fix views,urls,admins

--- apps/company/admin.py
+++ apps/company/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Company, Category
 
-# Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'user']
+    list_display = ('name', 'user', 'status')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ('name', 'is_subb')
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Category, CategoryAdmin)
