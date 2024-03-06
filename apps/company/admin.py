8886a9dbc436b7d3bda346165eb831c24d1000a1
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-03-07 02:17:00 +0330

    update time work

--- apps/company/admin.py
+++ apps/company/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company, Category
+from .models import Company, Category, WorkDate, WorkTime
 
 
 class CompanyAdmin(admin.ModelAdmin):
@@ -12,5 +12,15 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_subb')
 
 
+class WorkDateAdmin(admin.ModelAdmin):
+    list_display = ('date', 'company', 'active')
+
+
+class WorkTimeAdmin(admin.ModelAdmin):
+    list_display = ('work_date', 'active')
+
+
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(WorkDate, WorkDateAdmin)
+admin.site.register(WorkTime, WorkTimeAdmin)
--- apps/company/urls.py
+++ apps/company/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, WorkDateView, WorkTimeView
 
 app_name = 'company'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
-    # path('login/', )
+    path('work_time/<slug:company_slug>/', WorkDateView.as_view(), name='work_time'),
+    path('work_time/<slug:company_slug>/<str:date>/', WorkTimeView.as_view(), name='work_hours')
 ]
