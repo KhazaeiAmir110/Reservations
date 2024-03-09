3872e2dcc54a310a661c4c926ad82dbd19f2396e
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-03-09 20:49:29 +0330

    crate company

--- apps/user_dashboard/urls.py
+++ apps/user_dashboard/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import ProfileView
+from .views import ProfileView, CreateCompanyView
 
 app_name = 'user_dashboard'
 
 urlpatterns = [
     path('', ProfileView.as_view(), name='profile'),
-    # path('login/', UserLoginView.as_view(), name='login'),
+    path('create/', CreateCompanyView.as_view(), name='create'),
     # path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
