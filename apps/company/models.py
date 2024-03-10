6ece96e09507ebda8fdcf67801612eb1298a659c
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-03-10 07:40:47 +0330

    update models company

--- apps/company/models.py
+++ apps/company/models.py
@@ -1,3 +1,5 @@
+from django.utils.safestring import mark_safe
+from django.utils.text import slugify
 from apps.userauths.models import User
 from django.db import models
 from django.urls import reverse
@@ -21,19 +23,34 @@ class Category(models.Model):
         return reverse('home:category_filter', args=[self.slug])
 
 
+STATUS_COMPANY_CHOICES = (
+    ('pending', 'Pending'),
+    ('approved', 'Approved'),
+    ('rejected', 'Rejected')
+)
+
+
 class Company(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
     address = models.CharField(max_length=200)
     slug = models.SlugField(max_length=100, unique=True)
-    category = models.ManyToManyField(Category, related_name='companies', blank=True)
-    status = models.BooleanField(default=True)
-    image = models.FileField(upload_to='company/%Y/%m/')
+    category = models.ManyToManyField(Category, related_name='companies')
+    status = models.CharField(max_length=10, choices=STATUS_COMPANY_CHOICES, default='pending')
+    image = models.FileField(upload_to='company/%Y/%m/', blank=True)
     golden = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug is None:
+            self.slug = slugify(self.name) + '-' + str(self.user)
+        super(Company, self).save(*args, **kwargs)
+
+    def thumbnail(self):
+        return mark_safe("<img src='/static' width='100' height='100' style='object-fit:cover; border-radius:6px' />")
 
     class Meta:
         verbose_name_plural = "Companies"
