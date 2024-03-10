891489c9781ad56c5e7e37fa046940c91d649f4a
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-03-10 08:31:52 +0330

    update admin

--- apps/company/admin.py
+++ apps/company/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company, Category, WorkDate, WorkTime
+from .models import Company, Category, WorkDate, WorkTime, Booking
 
 
 class CompanyAdmin(admin.ModelAdmin):
@@ -20,7 +20,13 @@ class WorkTimeAdmin(admin.ModelAdmin):
     list_display = ('work_date', 'active')
 
 
+class BookingAdmin(admin.ModelAdmin):
+    search_fields = ['full_name', 'company']
+    list_display = ('full_name', 'company', 'active')
+
+
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(WorkDate, WorkDateAdmin)
 admin.site.register(WorkTime, WorkTimeAdmin)
+admin.site.register(Booking, BookingAdmin)
