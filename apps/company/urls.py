3bc412331dd21e7ad722bf83685ae095e1a5da6a
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-05-31 11:10:36 +0330

    update urls

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, CompanyDetailView
+from .views import HomeView, CompanyDetailView, CompanyListView, CompanyDetail
 
 app_name = 'company'
 
@@ -8,4 +8,10 @@ urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('detail/<slug:company_slug>/', CompanyDetailView.as_view(), name='detail-company'),
     path('detail/<slug:company_slug>/<str:date_time>/', CompanyDetailView.as_view(), name='work_hours')
+]
+
+# Baraato
+urlpatterns += [
+    path('baraato/', CompanyListView.as_view(), name='list-company'),
+    path('baraato/<slug:company_slug>/', CompanyDetail.as_view(), name='detail-company-baraato')
 ]
