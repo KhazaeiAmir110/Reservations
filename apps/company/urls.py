7b42a555c4c666800c695c498e0441a7b181c6d7
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-06-17 14:20:54 +0330

    add url and view

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 
-# from .view import HomeView, CompanyDetailView, CompanyListView, CompanyDetail
-#
-# app_name = 'company'
-#
-# urlpatterns = [
-#     path('', HomeView.as_view(), name='index'),
-#     path('detail/<slug:company_slug>/', CompanyDetailView.as_view(), name='detail-company'),
-#     path('detail/<slug:company_slug>/<str:date_time>/', CompanyDetailView.as_view(), name='work_hours')
-# ]
-#
-# # Baraato
-# urlpatterns += [
-#     path('baraato/', CompanyListView.as_view(), name='list-company'),
-#     path('baraato/<slug:slug>/', CompanyDetail.as_view(), name='detail-company-baraato')
-# ]
+from .views import CompanyListView
+
+app_name = 'company'
+
+# Baraato
+urlpatterns = [
+    path('', CompanyListView.as_view(), name='list-company'),
+    path('baraato/<slug:slug>/', CompanyListView.as_view(), name='detail-company-baraato')
+]
