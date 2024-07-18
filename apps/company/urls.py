ec23cf70c0d871d1f6aae31b533d93bf402c5f86
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-18 14:23:15 +0330

    fix bug for send code

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import CompanyListView, CompanyDetailView, PaymentView
+from .views import CompanyListView, CompanyDetailView, PaymentView, send_code
 
 app_name = 'company'
 
 # Baraato
 urlpatterns = [
+    path('send/', send_code, name='send-code'),
     path('', CompanyListView.as_view(), name='list-company'),
     path('<slug:slug>/', CompanyDetailView.as_view(), name='detail-company-baraato'),
     path('<slug:slug>/payment/', PaymentView.as_view(), name='payment'),
