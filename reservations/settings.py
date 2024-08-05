f22367c773918f36bcca0c531c24ab6180053f01
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-05 15:15:55 +0330

    add setting SessionAuthentication

--- reservations/settings.py
+++ reservations/settings.py
@@ -137,6 +137,9 @@ AUTH_USER_MODEL = 'userauths.User'
 
 # api
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+            'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
