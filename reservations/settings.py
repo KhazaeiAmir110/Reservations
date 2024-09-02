cc6f8647fd3f61aef51c9f5bb9b083c3a9521fa0
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-02 11:35:54 +0330

    fix setting authentication

--- reservations/settings.py
+++ reservations/settings.py
@@ -131,7 +131,6 @@ AUTH_USER_MODEL = 'userauths.User'
 # api
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
