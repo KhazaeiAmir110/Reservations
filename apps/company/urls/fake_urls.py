ee036be8af4482fd498633eea3f048fd558e54c9
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-10-31 20:06:20 +0330

    update urls

---
+++ apps/company/urls/fake_urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+
+from apps.company.api.fake_apis import (
+    CompanyBackOfficeTestViewSet, ReservationBackOfficeTestViewSet, PaymentBackOfficeTestViewSet
+)
+
+router = DefaultRouter()
+router.register(r'api/company', CompanyBackOfficeTestViewSet, basename='company-test')
+router.register(r'api/reservation', ReservationBackOfficeTestViewSet, basename='reservation-test')
+router.register(r'api/payment', PaymentBackOfficeTestViewSet, basename='payment-test')
+
+urlpatterns = router.urls
