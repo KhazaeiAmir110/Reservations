59171aaace61e900658a131fd0df1ec677d98dab
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-09 14:16:54 +0330

    update CustomPageNumberPagination

--- reservations/core/pagination.py
+++ reservations/core/pagination.py
@@ -1,7 +1,19 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 7
     page_size_query_param = 'page_size'
     max_page_size = 30
+
+    def get_paginated_response(self, data):
+        return Response({
+            'page_size': self.page_size,
+            'count_objects': self.page.paginator.count,
+            'count_pages': self.page.paginator.num_pages,
+            'current_page_number': self.page.number,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+        })
