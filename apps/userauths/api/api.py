68ef2f1e9904120aaf7c48f9db749ca31014ddce
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-09 19:23:14 +0330

    token implementation

--- apps/userauths/api/api.py
+++ apps/userauths/api/api.py
@@ -1,52 +1,26 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.sessions.models import Session
-from rest_framework import mixins
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
 from django.utils.translation import gettext_lazy as _
+from rest_framework import mixins, status
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 
 from apps.userauths.models import User
 from apps.userauths.serializers import (
-    UserRegisterSerializer, UserLoginSerializer, UserDashboardHeaderSerializer
+    UserRegisterSerializer, UserDashboardHeaderSerializer
 )
 
 
-# Authentication user
-class UserLoginVApi(GenericViewSet):
-    """
-        Login API for user authentication
-    """
-    serializer_class = UserLoginSerializer
-    authentication_classes = []
-    permission_classes = []
-
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = authenticate(
-            username=serializer.validated_data['username'],
-            password=serializer.validated_data['password']
-        )
-        if user is not None and user.is_active:
-            login(request, user)
-            return Response({'detail': _('Session login successful.')}, status=200)
-
-        raise AuthenticationFailed(detail=_("Incorrect authentication credentials."))
-
-
-class UserLogoutApi(GenericViewSet):
+class UserLogoutApi(APIView):
     """
         Logout API for user authentication
     """
-    serializer_class = UserLoginSerializer
-    authentication_classes = []
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
 
-    def create(self, request):
-        logout(request)
-        Session.objects.filter(session_key=request.session.session_key).delete()
-        return Response({})
+    def delete(self, request):
+        request.auth.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class UserRegisterApi(GenericViewSet):
@@ -77,6 +51,7 @@ class UserDashboardHeaderApi(mixins.ListModelMixin, GenericViewSet):
     """
         Api information User for header page 1
     """
+    permission_classes = [IsAuthenticated]
 
     queryset = User.objects.all()
     serializer_class = UserDashboardHeaderSerializer
--- apps/userauths/urls/backoffice.py
+++ apps/userauths/urls/backoffice.py
@@ -1,14 +1,18 @@
+from django.urls import path
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.routers import DefaultRouter
 
 from apps.userauths.api.api import (
-    UserLoginVApi, UserLogoutApi, UserRegisterApi, UserDashboardHeaderApi
+    UserLogoutApi, UserRegisterApi, UserDashboardHeaderApi
 )
 
 router = DefaultRouter()
-router.register(r'api/login', UserLoginVApi, basename='login')
-router.register(r'api/logout', UserLogoutApi, basename='logout')
 router.register(r'api/register', UserRegisterApi, basename='register')
-
 router.register(r'api/user_header', UserDashboardHeaderApi, basename='user_header')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('api/login/', ObtainAuthToken.as_view(), name='login'),
+    path('api/logout/', UserLogoutApi.as_view(), name='logout'),
+]
+
+urlpatterns += router.urls
