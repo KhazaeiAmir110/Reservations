45f815a0f1de9c6cde07ea4158ffb1b4d917747e
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-15 23:24:14 +0330

    add url SansHolidayDateTime

--- apps/company/urls/company.py
+++ apps/company/urls/company.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from apps.company.apis.company import (
     CompanyBackofficeViewSet, ListCompanySummaryBackofficeViewSet, HolidaysDateBackofficeViewSet,
-    SansConfigBackofficeViewSet
+    SansConfigBackofficeViewSet, SansHolidayDateTimeBackofficeViewSet
 )
 
 router = DefaultRouter()
@@ -10,5 +10,7 @@ router.register(r'api/company', CompanyBackofficeViewSet, basename='company_base
 router.register(r'api/listCompanySummary', ListCompanySummaryBackofficeViewSet, basename='company_summary')
 router.register(r'api/holidaysdate', HolidaysDateBackofficeViewSet, basename='company_holidaysdate')
 router.register(r'api/sansconfig', SansConfigBackofficeViewSet, basename='company_sansconfig')
+router.register(r'api/sansholidaydatetime', SansHolidayDateTimeBackofficeViewSet,
+                basename='company_sansholidaydatetime')
 
 urlpatterns = router.urls
