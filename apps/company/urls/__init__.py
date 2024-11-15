fb36a446c9c14be034c08c1b8927543bc008909a
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-15 18:19:40 +0330

    update url reservation

--- apps/company/urls/__init__.py
+++ apps/company/urls/__init__.py
@@ -1,8 +1,10 @@
 from .company import urlpatterns as company_url
+from .reservation import urlpatterns as reservation_url
 from .urls_templates import urlpatterns as templates_urlpatterns
 
 app_name = 'company'
 
 urlpatterns = []
 urlpatterns += company_url
+urlpatterns += reservation_url
 urlpatterns += templates_urlpatterns
