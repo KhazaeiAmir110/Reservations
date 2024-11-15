30f16a616ed7b370a5c91b29d8e76fbadd8864ca
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-15 21:58:58 +0330

    add url HolidaysDate

--- apps/company/urls/company.py
+++ apps/company/urls/company.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
 from apps.company.apis.company import (
-    CompanyBackofficeViewSet, ListCompanySummaryBackofficeViewSet
+    CompanyBackofficeViewSet, ListCompanySummaryBackofficeViewSet, HolidaysDateBackofficeViewSet
 )
 
 router = DefaultRouter()
 router.register(r'api/company', CompanyBackofficeViewSet, basename='company_base')
 router.register(r'api/listCompanySummary', ListCompanySummaryBackofficeViewSet, basename='company_summary')
+router.register(r'api/holidaysdate', HolidaysDateBackofficeViewSet, basename='company_holidaysdate')
 
 urlpatterns = router.urls
